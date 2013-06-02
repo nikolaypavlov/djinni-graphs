3570727aa65d86c4300dd845ee3587bffb8e30b9
Nikolay Pavlov <me@nikolaypavlov.com>
2013-06-02 18:46:52 +0300

    Data set filtration and Supply vs Demand Boxplot added

--- code/data_import.R
+++ code/data_import.R
@@ -2,7 +2,6 @@
 # Data Import and Cleaning
 #
 setwd('~/repos/djinni-graphs')
-#library(reshape)
 
 candidates <- read.csv("data/candidates.csv")
 pokes <- read.csv("data/pokes.csv")
@@ -10,30 +9,55 @@ queries <- read.csv("data/searches.csv")
 
 # Join "PM" and "Project Manager" skills
 pm <- c("PM", "Project Manager", "Manager", "Product Manager")
-levels(candidates$Skill)[levels(candidates$Skill) %in% pm] <- "PM"
-levels(pokes$Keyword)[levels(pokes$Keyword) %in% pm] <- "PM"
+levels(candidates$Skill)[levels(candidates$Skill) %in% pm] <- "Project Manager"
+levels(pokes$Keyword)[levels(pokes$Keyword) %in% pm] <- "Project Manager"
 
 # Join "Rails" and "Ruby" skills
 ruby <- c("Rails", "Ruby")
 levels(candidates$Skill)[levels(candidates$Skill) %in% ruby] <- "Ruby"
 levels(pokes$Keyword)[levels(pokes$Keyword) %in% ruby] <- "Ruby"
 
+# Join "HTML", "JavaScript", "Front-end" skills
+# front_end <- c("HTML", "JavaScript", "Front-end")
+# levels(candidates$Skill)[levels(candidates$Skill) %in% front_end] <- "Front-end"
+# levels(pokes$Keyword)[levels(pokes$Keyword) %in% front_end] <- "Front-end"
+
+# Change Front-end to JavaScript in pokes
+levels(pokes$Keyword) <- sub("Front-end", "JavaScript", levels(pokes$Keyword))
+
+# Remain only candidates which have online or stopped status
+candidates <- candidates[which(candidates$Status == "online" | candidates$Status == "stopped"), ]
+
+# Update factor levels for skills
+candidates$Skill <- factor(candidates$Skill)
+
 # Remove skills with less then 10 candidates
-levels <- levels(candidates$Skill)
-for (i in 1:length(levels)) {
-  if (nrow(candidates[candidates$Skill == levels[i],]) < 30) {
-    candidates <- candidates[-which(candidates$Skill == levels[i]), ]
-  }
-}
+# levels <- levels(candidates$Skill)
+# for (i in 1:length(levels)) {
+#   if (nrow(candidates[candidates$Skill == levels[i],]) < 10) {
+#     candidates <- candidates[-which(candidates$Skill == levels[i]), ]
+#   }
+# }
+
+# Choose skills according to Max set
+candidates <- candidates[which(candidates$Skill == "JavaScript" | candidates$Skill == "iOS" |
+                                 candidates$Skill == "QA" | candidates$Skill == "PHP" | 
+                                 candidates$Skill == "C++" | candidates$Skill == "Java" | 
+                                 candidates$Skill == "Android" | candidates$Skill == "Lead" | 
+                                 candidates$Skill == "Python" | candidates$Skill == ".NET" |
+                                 candidates$Skill == ".NET" | candidates$Skill == "Project Manager" |
+                                 candidates$Skill == "Ruby"), ]
 
 # Remove empty skills and keywords
-candidates <- candidates[-which(candidates$Skill == ""), ]
-pokes <- pokes[-which(pokes$Keyword == ""), ]
+# candidates <- candidates[-which(candidates$Skill == ""), ]
+# pokes <- pokes[-which(pokes$Keyword == ""), ]
+
+# Update factor levels for skills
 candidates$Skill <- factor(candidates$Skill)
 
 # Select the same keywords as filtered candidate skills
 skill <- intersect(levels(pokes$Keyword), levels(candidates$Skill))
 pokes <- subset(pokes, Keyword %in% skill)
 
-# Remove empty skills and keywords
+# Update factor levels for keywords
 pokes$Keyword <- factor(pokes$Keyword)
--- code/exploratory.R
+++ code/exploratory.R
@@ -1,19 +1,25 @@
+setwd('~/repos/djinni-graphs')
 source("code/data_import.R")
 
-# On average how many pokes for each skill does a candidate receive?
-plot(candidates$Skill, candidates$PokesTotal, las=2)
+library(ggplot2)
 
-# How many search queries for each skill do we have?
-plot(queries$Keyword)
+#
+# How many pokes for each skill does a candidate receive?
+#
+# qplot(data=candidates, x=Skill, y=PokesTotal, geom="boxplot")
 
-# On average how many pokes does a candidate receive if he is looking for fulltime,
-# fulltime+relocate or freelance job?
-par(mfrow=c(1, 1), mar=c(6, 4, 2, 2))
-plot(candidates$Employment, candidates$PokesTotal, las=2)
-
+#
 # Supply vs Demand
-par(mfrow=c(2, 1), mar=c(5, 4, 4, 2))
-plot(candidates$Skill, candidates$Salary, las=2, main="Dev's demand", ylim=c(0, 8000))
-plot(pokes$Keyword, pokes$Salary, las=2, main="Corps supply", ylim=c(0, 8000))
+#
+demand <- cbind(pokes, as.factor(rep("Предложения компаний", nrow(pokes))))
+names(demand) <- c("Skill", "City", "Salary", "Type")
 
-distance <- dist(candidates$PokesTotal)
+supply <- cbind(candidates[,c("Skill", "City", "Salary")], as.factor(rep("Запросы кандидатов", nrow(candidates))))
+names(supply) <- c("Skill", "City", "Salary", "Type")
+
+demand_supply <- rbind(demand, supply)
+
+demand_supply_plot <- ggplot(demand_supply, 
+                             aes(x = Skill, y = Salary, fill = Type)) + 
+  geom_boxplot() + scale_y_continuous(breaks=seq(0, 5500, 250)) + 
+  coord_cartesian(ylim=c(0, 5500)) + xlab("") + ylab("Зарплата") + labs(title="Предложения компаний VS Запросы кандидатов")
